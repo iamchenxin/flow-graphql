793c4cf5dfded7c45d097e83ed0603f43a449ce3
iamchenxin <iamchenxin@gmail.com>
2016-05-24 18:08:20 +0000

    add hierarchy option for printSchema

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # flow-GraphQL.js
 
 Add flow-type outputs for [Graphql v0.6.0](https://github.com/graphql/graphql-js)
- 
+
+# Changelog
+v0.6.3 (Graphql v0.6.0)
+printSchema to this
+```
+type printStyle = 'alphabet' | 'hierarchy';
+export function printSchema(
+  schema: GraphQLSchema,style: printStyle = 'alphabet'): string {
+  switch (style) {
+    case 'hierarchy':
+      return printFineSchema(schema, n => !isSpecDirective(n));
+    case 'alphabet':
+    default:
+      return printFilteredSchema(schema, n => !isSpecDirective(n),
+      isDefinedType);
+  }
+}
+```
--- utilities/schemaPrinter.js
+++ utilities/schemaPrinter.js
@@ -4,6 +4,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _slicedToArray2 = require('babel-runtime/helpers/slicedToArray');
+
+var _slicedToArray3 = _interopRequireDefault(_slicedToArray2);
+
+var _map2 = require('babel-runtime/core-js/map');
+
+var _map3 = _interopRequireDefault(_map2);
+
+var _getIterator2 = require('babel-runtime/core-js/get-iterator');
+
+var _getIterator3 = _interopRequireDefault(_getIterator2);
+
+var _from = require('babel-runtime/core-js/array/from');
+
+var _from2 = _interopRequireDefault(_from);
+
+var _toConsumableArray2 = require('babel-runtime/helpers/toConsumableArray');
+
+var _toConsumableArray3 = _interopRequireDefault(_toConsumableArray2);
+
 var _keys = require('babel-runtime/core-js/object/keys');
 
 var _keys2 = _interopRequireDefault(_keys);
@@ -39,9 +59,19 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 
 function printSchema(schema) {
-  return printFilteredSchema(schema, function (n) {
-    return !isSpecDirective(n);
-  }, isDefinedType);
+  var style = arguments.length <= 1 || arguments[1] === undefined ? 'alphabet' : arguments[1];
+
+  switch (style) {
+    case 'hierarchy':
+      return printFineSchema(schema, function (n) {
+        return !isSpecDirective(n);
+      });
+    case 'alphabet':
+    default:
+      return printFilteredSchema(schema, function (n) {
+        return !isSpecDirective(n);
+      }, isDefinedType);
+  }
 }
 
 function printIntrospectionSchema(schema) {
@@ -189,4 +219,307 @@ function printInputValue(arg) {
 
 function printDirective(directive) {
   return 'directive @' + directive.name + printArgs(directive) + ' on ' + directive.locations.join(' | ');
+}
+
+function printFineSchema(schema) {
+  var directiveFilter = arguments.length <= 1 || arguments[1] === undefined ? function (n) {
+    return !isSpecDirective(n);
+  } : arguments[1];
+
+  var directives = schema.getDirectives().filter(function (directive) {
+    return directiveFilter(directive.name);
+  });
+  var typeMap = schema.getTypeMap();
+  var orderedNames = getOrderedNamesBySchema(schema);
+  var types = orderedNames.map(function (orderedName) {
+    return typeMap[orderedName];
+  });
+  return [directives.map(printDirective)].concat(types.map(printType), printSchemaDefinition(schema)).join('\n\n') + '\n';
+}
+
+function getOrderedNamesBySchema(schema) {
+  var typeMap = schema.getTypeMap();
+  var rootQuery = schema.getQueryType();
+  var definedTypeNames = (0, _keys2.default)(typeMap).filter(isDefinedType);
+
+  // use a big number 999999 to save some condition operator ~_~
+  // todo should modify the magic 999999
+  var typeNamesMap = arrayToMap(definedTypeNames, 99999);
+  // give each type used by 'Query' a level number
+  var queryMaps = levelTypeNames(rootQuery.name, typeNamesMap, schema);
+  var unLeveledNamesMap = queryMaps.unLeveledNamesMap;
+  var leveledNamesMap = queryMaps.leveledNamesMap;
+  var orderedNames = flatNamesMapToArray(leveledNamesMap);
+
+  var rootMutation = schema.getMutationType();
+  if (rootMutation) {
+    // give level number to the rest unLeveled type
+    // which used by Mutations
+    var restNamesMap = levelTypeNames(rootMutation.name, unLeveledNamesMap, schema);
+    var orderedMutations = flatNamesMapToArray(restNamesMap.leveledNamesMap);
+
+    orderedNames = [].concat((0, _toConsumableArray3.default)(orderedNames), orderedMutations);
+    unLeveledNamesMap = restNamesMap.unLeveledNamesMap;
+  }
+
+  // todo throw a error .. should have none unknown type
+  var theNamesIDontKnown = flatNamesMapToArray(unLeveledNamesMap);
+  if (theNamesIDontKnown.length > 0) {
+    orderedNames = [].concat(theNamesIDontKnown, (0, _toConsumableArray3.default)(orderedNames));
+  }
+  return orderedNames;
+}
+
+function flatNamesMapToArray(leveledNamesMap) {
+  var levelToNamesMap = flipMap(leveledNamesMap);
+  var nameLevels = (0, _from2.default)(levelToNamesMap.keys());
+  nameLevels.sort(function (pre, next) {
+    return next - pre;
+  });
+  var orderedNames = [];
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = (0, _getIterator3.default)(nameLevels), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var level = _step.value;
+
+      var levelNames = levelToNamesMap.get(level);
+      if (levelNames) {
+        // sort the same level names . to get a certainly order.
+        levelNames.sort(function (name1, name2) {
+          return name1.localeCompare(name2);
+        });
+        orderedNames = orderedNames.concat(levelNames);
+      } else {
+        throw new Error('printFineSchema.getOrderedNamesFromMap:\n      level[' + level + '] have no names,it should have');
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return orderedNames;
+}
+
+// calculate level values for each type names by their reference to each other
+function levelTypeNames(rootName, namesMapToBeLeveled, schema) {
+  var typeMap = schema.getTypeMap();
+  var unLeveledNamesMap = new _map3.default(namesMapToBeLeveled);
+  var leveledNamesMap = new _map3.default();
+  // use a map to watch circle ref,Depth-first search
+  var circleRef = new _map3.default();
+
+  unLeveledNamesMap.delete(rootName);
+  leveledNamesMap.set(rootName, 0);
+  _levelTypeNames(rootName, 1);
+  function _levelTypeNames(thisName, childLevel) {
+    var childrenNames = getRefedTypes(typeMap[thisName]);
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      for (var _iterator2 = (0, _getIterator3.default)(childrenNames), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var childName = _step2.value;
+
+        var currentLevel = leveledNamesMap.get(childName);
+        if ( // meet a circle ref,skip
+        circleRef.get(childName) ||
+        // this type is not belong to current process,skip
+        namesMapToBeLeveled.get(childName) === undefined ||
+        //  if [the level of leveled Name] >= [current level]
+        // must skip,level is always up~ no downgrade
+        currentLevel && currentLevel >= childLevel) {
+          continue;
+        }
+        circleRef.set(childName, childLevel);
+
+        leveledNamesMap.set(childName, childLevel);
+        unLeveledNamesMap.delete(childName);
+        _levelTypeNames(childName, childLevel + 1);
+
+        circleRef.delete(childName);
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return) {
+          _iterator2.return();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
+    }
+  }
+
+  return { unLeveledNamesMap: unLeveledNamesMap, leveledNamesMap: leveledNamesMap };
+}
+
+// always return an array ,if get none,just return []
+// three sources of reference from a type.
+// field itself, args of a fields,interface
+function getRefedTypes(type) {
+  var refedTypeNames = [];
+  if (!isDefinedType(type.name) ||
+  // as i known ~_~,if there is no Fields
+  // this type can not ref other types inside
+  !(type.getFields instanceof Function)) {
+    return refedTypeNames;
+  }
+
+  var fields = type.getFields();
+  // 1/2 get refed type name from fields
+  (0, _keys2.default)(fields).map(function (fieldKey) {
+    return fields[fieldKey];
+  }).filter(function (field) {
+    return isDefinedType(getTypeName(field.type));
+  }).map(function (field) {
+    refedTypeNames = refedTypeNames.concat(
+    // 1. get type name from args of a field
+    // in javascript, can not use instanceof to check a String type!
+    // must use typeof!
+    field.args.map(function (arg) {
+      return getDefinedTypeNameByType(arg.type);
+    }).filter(function (value) {
+      return typeof value === 'string';
+    }),
+    // 2. get type name from field itself
+    getDefinedTypeNameByType(field.type) || []);
+  });
+
+  // 3. get type name from interfaces
+  if (type.getInterfaces instanceof Function) {
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+      for (var _iterator3 = (0, _getIterator3.default)(type.getInterfaces()), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        var interfaceType = _step3.value;
+
+        refedTypeNames.push(interfaceType.name);
+      }
+    } catch (err) {
+      _didIteratorError3 = true;
+      _iteratorError3 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion3 && _iterator3.return) {
+          _iterator3.return();
+        }
+      } finally {
+        if (_didIteratorError3) {
+          throw _iteratorError3;
+        }
+      }
+    }
+  }
+  return refedTypeNames;
+}
+
+function arrayToMap(_array, defaultValue) {
+  var _map = new _map3.default();
+  var _iteratorNormalCompletion4 = true;
+  var _didIteratorError4 = false;
+  var _iteratorError4 = undefined;
+
+  try {
+    for (var _iterator4 = (0, _getIterator3.default)(_array), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+      var v = _step4.value;
+
+      _map.set(v, defaultValue);
+    }
+  } catch (err) {
+    _didIteratorError4 = true;
+    _iteratorError4 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion4 && _iterator4.return) {
+        _iterator4.return();
+      }
+    } finally {
+      if (_didIteratorError4) {
+        throw _iteratorError4;
+      }
+    }
+  }
+
+  return _map;
+}
+
+function flipMap(_srcMap) {
+  var _outMap = new _map3.default();
+  var _iteratorNormalCompletion5 = true;
+  var _didIteratorError5 = false;
+  var _iteratorError5 = undefined;
+
+  try {
+    for (var _iterator5 = (0, _getIterator3.default)(_srcMap), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
+      var _step5$value = (0, _slicedToArray3.default)(_step5.value, 2);
+
+      var oldKey = _step5$value[0];
+      var vToKey = _step5$value[1];
+
+      var subArray = _outMap.get(vToKey);
+      if (subArray) {
+        subArray.push(oldKey);
+      } else {
+        _outMap.set(vToKey, [oldKey]);
+      }
+    }
+  } catch (err) {
+    _didIteratorError5 = true;
+    _iteratorError5 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion5 && _iterator5.return) {
+        _iterator5.return();
+      }
+    } finally {
+      if (_didIteratorError5) {
+        throw _iteratorError5;
+      }
+    }
+  }
+
+  return _outMap;
+}
+
+function getTypeName(type) {
+  var typeString = type.constructor.name;
+  var name = type.name;
+  if (typeString === 'GraphQLNonNull' || typeString === 'GraphQLList') {
+    name = getTypeName(type.ofType);
+  }
+  if (name === undefined && isDefinedType(type)) {
+    // if still can not get name,
+    // this type must be something i dont known ,throw to learn
+    throw new Error('Unknown type its javascript class is\n      [ [' + type.constructor.name + '] ' + type.ofType.constructor.name + ']');
+  }
+  return name;
+}
+
+function getDefinedTypeNameByType(TypeObj) {
+  var typeName = getTypeName(TypeObj);
+  if (isDefinedType(typeName)) {
+    return typeName;
+  }
+  return null;
 }
